d5e00c2b723463974c9555b330cc3e11f1448fc2
Roy McFarland <roy.mcfarland@gmail.com>
2015-01-20 15:57:20 -0700

    Added new ng-controller called PanelController to clean-up HTML

--- app.js
+++ app.js
@@ -11,7 +11,8 @@
 		name: 'Pentagonal Gem',
 		price: 5.95,
 		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate fugit vero libero odit aliquam impedit omnis consectetur fugiat voluptas ab maxime ducimus hic aliquid ut non optio pariatur, temporibus rerum.',
-		canPurchase: true
+		canPurchase: true,
+		soldOut: false
 	}
 	];
 
@@ -19,6 +20,18 @@
 
 	app.controller('StoreController', function(){
 		this.products = gems;
+	});
+
+	app.controller('PanelController', function(){
+		this.tab = 1;
+		
+		this.selectTab = function(setTab){
+			this.tab = setTab || 0;
+		};
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
 	});
 
 })();
