98fb9bb52b07e4a26f706679c13a033954ff67a7
Roy McFarland <roy.mcfarland@gmail.com>
2015-01-22 11:44:13 -0700

    Added <pproduct-gallery custom directive

--- app.js
+++ app.js
@@ -69,14 +69,45 @@
 		};
 	});
 
-	app.directive("productSpecs", function() {
+	app.directive('productSpecs', function() {
 	  return {
-	    restrict:"A",
+	    restrict: 'A',
 	    templateUrl: "product-specs.html"
 	  };
 	});
 
+	app.directive('productPanels', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function(){
+				this.tab = 1;
+				
+				this.selectTab = function(setTab){
+					this.tab = setTab || 0;
+				};
 
+				this.isSelected = function(checkTab){
+					return this.tab === checkTab;
+				};
+			},
+			controllerAs: 'panels'
+		};
+	});
+
+	app.directive('productGallery', function(){
+	  return {
+	    restrict: 'E',
+	    templateUrl: 'product-gallery.html',
+	    controller: function(){
+	        this.current = 0;
+	  			this.setCurrent = function(imageNumber){
+	    		this.current = imageNumber || 0;
+	        };
+	     },
+	    controllerAs: 'gallery'
+	  };
+	});
 
 
 
